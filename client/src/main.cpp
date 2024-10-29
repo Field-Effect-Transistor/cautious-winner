7c0aae0ed63dfdfa79241a947105d1b31515ff35
Field-Effect-Transistor <145497915+Field-Effect-Transistor@users.noreply.github.com>
2024-10-29 16:43:37 +0300

    themesAdded

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -2,13 +2,10 @@
 #include <QLabel>
 #include <QWidget>
 
-#include <array>
-
 #include "auth/startWidget.hpp"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    //app.setStyleSheet("QToolTip { color: #6C0808; }");
     
     startWidget w;
     w.show();
